f89e9b5c107452a0149573f06de1d8c4a13ee0c0
yaliqin <44327827+yaliqin@users.noreply.github.com>
2018-12-04 14:57:07 -0800

    Update socket_client.py

--- socket_client.py
+++ socket_client.py
@@ -1,16 +1,20 @@
 def socket_client():
     import socket
-    HOST = '192.168.1.135'
+    HOST = '192.168.1.35'
     PORT = 8001
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
 
     while True:
-        cmd = raw_input("Please input msg:")
+
+        str ="Please input msg:"
+        cmd = str.encode('utf-8')
         s.send(cmd)
         data = s.recv(1024)
         print(data)
 
         #s.close()
+
+
 socket_client()
